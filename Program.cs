9aa80a43547736e1028a727b94cdb9eee3d19933
Veeresh124 <76420334+Veeresh124@users.noreply.github.com>
2020-12-21 13:11:01 +0530

    main method added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList= new List<Student>();
+            Student std = new Student();
+            while (true)
+            {
+                Console.WriteLine("enter your choice:1.Add 2.display 3.delete 4.update 5.sort 6.search");
+                var choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: std.Add(); break;
+                    case 2: std.Display(); break;
+                    case 3: std.Delete(); break;
+                    case 4: std.Update(); break;
+                    case 5: std.Sort(); break;
+                    case 6: std.Search(); break;
+                }
+            }
+        }
+    }
+}
